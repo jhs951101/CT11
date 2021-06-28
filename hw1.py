3b99af475d92a80f9f4387f1bccfd98772b624ad
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-06-28 19:22:00 +0900

    m

---
+++ hw1.py
@@ -0,0 +1,35 @@
+﻿def computing(a, b):
+    if a > b:
+        num1 = a
+        num2 = b
+    else:
+        num1 = b
+        num2 = a
+
+    result = {}
+    result['합'] = num1+num2
+    result['차'] = num1-num2
+    result['곱'] = num1*num2
+    result['몫'] = num1//num2
+
+    return result
+
+nums = []
+
+while True:
+    int1 = int(input('0 이 아닌 정수 입력  : '))
+
+    if int1 == 0:
+        nums.clear()
+    else:
+        nums.append(int1)
+
+    if len(nums) >= 2:
+        break
+
+print('num1= ', nums[0], ' num2= ', nums[1])
+
+dict1 = computing(nums[0], nums[1])
+
+for key in dict1:
+    print(key, ' : ', dict1[key], end = '      ')
---
+++ hw2.py
@@ -0,0 +1,37 @@
+﻿def gcd(num1, num2, *nums):
+    if num1 > num2:
+        min1 = num2
+    else:
+        min1 = num1
+
+    for i in range(0, len(nums), 1):
+        if min1 > nums[i]:
+            min1 = nums[i]
+
+    stack = []
+
+    for n in range(1, min1+1, 1):
+        beZero = True
+
+        if num1%n != 0 or num2%n != 0:
+            beZero = False
+
+        if beZero:
+            for i in range(0, len(nums), 1):
+                if nums[i]%n != 0:
+                    beZero = False
+                    break
+
+            if beZero:
+                stack.insert(0, n)
+
+    return stack[0]
+
+list1 = []
+
+for i in range(0, 4, 1):
+    list1.append( int(input('정수' + str(i+1) + ' 입력  :  ')) )
+
+print( str(list1[0]) + ',  ' + str(list1[1]) + '의 최대공약수  : ', gcd(list1[0], list1[1]) )
+print( str(list1[0]) + ',  ' + str(list1[1]) + ',  ' + str(list1[2]) + '의 최대공약수  : ', gcd(list1[0], list1[1], list1[2]) )
+print( str(list1[0]) + ',  ' + str(list1[1]) + ',  ' + str(list1[2]) + ',  ' + str(list1[3]) + '의 최대공약수  : ', gcd(list1[0], list1[1], list1[2], list1[3]) )
